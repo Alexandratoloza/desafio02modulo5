e251849c154d7eb6bd082a9f526b841a58b1315e
Alexandra Toloza <toloza.ale1992@gmail.com>
2024-04-12 11:33:53 -0400

    añadiendo

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import { engine } from 'express-handlebars';
 
-const app = express()
+const app = express();
 
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-res.json({ok: true})
-})
+    res.render('home');
+});
 
 app.listen(5001, () => console.log(`servidor encendido http://localhost:${5001}`))
 
