bbb9bb89f73a91e3796d19cc16a959747c0e89a2
Alexandra Toloza <toloza.ale1992@gmail.com>
2024-04-12 11:08:56 -0400

    cambios2

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+import express from 'express'
 
+const app = express()
+
+
+app.get('/', (req, res) => {
+res.json({ok: true})
+})
+
+app.listen(5001, () => console.log(`servidor encendido http://localhost:${5001}`))
 
