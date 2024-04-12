a610eced689e691ea01de8f1fec852d82a25e984
Alexandra Toloza <toloza.ale1992@gmail.com>
2024-04-12 14:53:03 -0400

    actualizados

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 import express from 'express'
 import { engine } from 'express-handlebars';
 
+import { services } from './data/service.data.js'
+
 const app = express();
 
+
+//public directorio
+
+app.use(express.static('public')) 
+
+
+app.use('/css', express.static('node_modules/bootstrap/dist/css'))
+app.use('/css', express.static('node_modules/bootstrap/dist/js'))
+app.use('/css', express.static('node_modules/jquery/dist/jquery.min.js'))
+
+//handlebars
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {title: "Home Page 2.0"});
 });
 
 app.get('/services', (req, res) => {
-    res.render('services');
+    res.render('services', {services: services});
 });
+
+app.get('/services', (req, res) => {
+
+    const nameUrl = req.params.name
+
+    const service = services.find((item) => item.url === `/services/${nameUrl}`)
+    return res.render('service', {service})
+})
 
 app.listen(5001, () => console.log(`servidor encendido http://localhost:${5001}`))
 
