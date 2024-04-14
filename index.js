f50beb63de8450880d39ff78e6dddc8a7cb398fa
Alexandra Toloza <toloza.ale1992@gmail.com>
2024-04-14 15:51:59 -0400

    terminando

--- index.js
+++ index.js
@@ -2,38 +2,52 @@ import express from 'express'
 import { engine } from 'express-handlebars';
 
 import {services} from './data/service.data.js'
+import path from 'path'
 
 const app = express();
 
+const __dirname = import.meta.dirname;
 
 //public directorio
 
-app.use(express.static('public')) 
+app.use(express.static(path.join(__dirname, '/public')) )
 
 
-app.use('/css', express.static('node_modules/bootstrap/dist/css'))
-app.use('/css', express.static('node_modules/bootstrap/dist/js'))
-app.use('/css', express.static('node_modules/jquery/dist/js'))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 //handlebars
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     res.render('home', {title: "Home Page 2.0"});
 });
 
 app.get('/services', (req, res) => {
-    res.render('services', {services: services});
+  return  res.render('services', {services: services});
 });
 
-app.get ('/services/:name' (req, res) =>{ 
+app.get ('/services/:name', (req, res) => { 
 
-    console.log (req.params.name)
+   const nameURL = req.params.name
 
-    res.json({name: req.params.name})
+   const service = services.find((item) => item.url === `/services/${nameURL}`)
+
+    if (!service){
+        return res.render('404', {title: "Pagina no encontrada"})
+    }
+   return res.render('service', {service})
 },)
+
+
+// 
+app.get ('/', (req, res) => {
+    return res.status(404).render('404', {title: "Pagina no encontrada"})
+
+})
 
 
 
