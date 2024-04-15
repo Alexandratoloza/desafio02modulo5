bbc106ad2fa400ca74a607bbdacf106e3aca3af8
Alexandra Toloza <toloza.ale1992@gmail.com>
2024-04-15 12:03:06 -0400

    cambios prodcuts

--- routes/product.route.js
+++ routes/product.route.js
@@ -11,16 +11,23 @@ const router = Router ()
 router.get('/', (req, res) => {
 ///leer queris
 
-const {order} = req.query
+const {order, search, limit} = req.query
+
 if (order === 'asc'){
     products.sort((a, b ) => {a.price - b.price })
 }
 
-
+if (order === 'desc'){
+    products.sort((a, b ) => {b.price - a.price })
+}
     return  res.render('products', {products: products});
   });
   
 
+router.get ('/add', (req, res) => {
+
+    res.render('products', {products})
+})
 
 
 export default router;
